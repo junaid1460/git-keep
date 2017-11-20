07ac688f24c1207277466d9b12110040dc583f24
junaid <junaid1460@gmail.com>
2017-11-20 18:16:59 +0530

    Create : Error message related functions

---
+++ src/error.sh
@@ -0,0 +1,9 @@
+error () {
+    
+    color $Color_Off
+    print "Usage : doc <option>\n"
+    print "Options :"
+    print "add\t-a\tAdd new information"
+    print "undo\t-u\tUndo the last add"
+    echo 
+}
