a55d31ff68fb2257d1166791f30fbc8d7b1ab0ed
junaid <junaid1460@gmail.com>
2017-11-23 12:11:50 +0530

    C(src/functions.ls.sh) : List info implementation file

---
+++ src/functions.ls.sh
@@ -0,0 +1,16 @@
+# List all informations
+
+list_info () {
+    messages="$(cat $file_info)"
+    count=0
+    IFS=$'\n'
+    for i in $messages;do
+        colori $Green
+        echo -n "[$count] "
+        colori $Off
+        IFS=, read -r file commit <<< "$i"
+        echo $commit
+        count=$(($count+1))
+        IFS=$'\n'
+    done
+}
