acd9d7b351532ad31f9fdba514b8713a852294de
junaid <junaid1460@gmail.com>
2017-11-26 12:51:45 +0530

    M(src/error.sh) : Beautify help message

--- src/error.sh
+++ src/error.sh
@@ -3,7 +3,8 @@ error () {
     color $Color_Off
     print "Usage : doc <option>\n"
     print "Options :"
-    print "add\t-a\tAdd new information"
-    print "undo\t-u\tUndo the last add"
+    print $Green"add\t\ta"$BWhite"\tAdd new file"
+    # print $Green"undo\t\tu"$BWhite"\tUndo the last add"
+    print $Green"list-files\tls"$BWhite"\tlist all stored files"
     echo 
 }
