5bae0f8ae9071904f1479666de43936903157ab9
junaid <junaid1460@gmail.com>
2017-12-02 17:08:23 +0530

    M : add more help message

--- src/error.sh
+++ src/error.sh
@@ -1,10 +1,17 @@
 error () {
     
     color $Color_Off
-    print "Usage : doc <option>\n"
+    print "Usage : doc <repo> <option> [parameter]\n"
     print "Options :"
     print $Green"add\t\ta"$BWhite"\tAdd new file"
     # print $Green"undo\t\tu"$BWhite"\tUndo the last add"
     print $Green"list-files\tls"$BWhite"\tlist all stored files"
+    print $Green"edit\t\te"$BWhite"\tedit files"
+    colori $White
+    print "\nparameters :"
+    print "1. edit/e <parameter>"
+    print "\theader\tedit header file"
+    print "\tfooter\tedit footer file"
+    print "\t[0-9]+\tedit with file number (number should be from \"list-files\" option)"
     echo 
 }
