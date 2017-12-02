d4314b8078269b373d11e20a3f8091b4d4e415c4
junaid <junaid1460@gmail.com>
2017-12-02 14:57:02 +0530

    M : Add function to read information file and print links to file. -> print header, content, footer to README

--- src/functions.update.sh
+++ src/functions.update.sh
@@ -10,10 +10,24 @@ getfooter () {
     echo "MIT"
 }
 
+printInfoAsLinks () {
+    file="$(cat $info_file)"
+    IFS=$'\n'
+    for line in $file;do
+        IFS=, read -r file commit <<< "$line"
+        echo " - [$commit]($files_dir/$file)"
+    done
+}
+
 updateREADME () {
     # header
     header="$(getheader)"
-    
+    echo $header > $readmefile
+    # content
+    content="$(printInfoAsLinks)"
+    echo $content >> $readmefile
+
     # footer
     footer="$(getfooter)"
+    echo $footer >> $footer
 }
