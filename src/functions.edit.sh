890e36bf418c9c04373553affe142cf3157abb75
junaid <junaid1460@gmail.com>
2017-12-08 15:11:42 +0530

    C : function to edit fule with number and update infirmation catalog

---
+++ src/functions.edit.sh
@@ -0,0 +1,53 @@
+# edit file with number
+
+updateInfoFile () { # filenumber, commitmessage, filename
+    number=$1
+    commit=$2
+    file=$3
+    messages="$(cat $info_file)"
+    IFS=$'\n'
+    count=0
+    rm $info_file
+    for line in $messages;do
+        if [ "$count" == "$number" ]
+        then
+            echo "$file,$commit" >> $info_file
+        else
+            echo "$line" >> $info_file
+        fi
+        count=$(($count + 1))
+    done
+}
+
+editThis () { # filename, commitmessage, filenumber
+    file=$1
+    commit=$2
+    number=$3
+    edit "$files_dir/$file"
+    echo $commit > $commit_temp_file
+    edit $commit_temp_file
+    newcommit="$(cat $commit_temp_file)"
+    updateInfoFile $number $newcommit $file
+    rm $commit_temp_file
+    commit? "Altered file : $commit"
+}
+
+
+editWithNumber () {
+    number=$1
+    messages="$(cat $info_file)"
+    count=0
+    IFS=$'\n'
+    for i in $messages;do
+        IFS=, read -r file commit <<< "$i"
+        if [ "$count" == "$number" ]
+        then 
+            editThis $file $commit $number
+            return
+        fi
+        count=$(($count+1))
+        IFS=$'\n'
+    done
+    colori $Red 
+    print "File not found"
+}
