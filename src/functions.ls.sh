6f33f9d977f947aa0fbd359f4efd19ea60987a52
junaid <junaid1460@gmail.com>
2017-12-15 12:30:47 +0530

    M : removed todo comments

--- src/functions.ls.sh
+++ src/functions.ls.sh
@@ -14,9 +14,3 @@ list_info () {
         IFS=$'\n'
     done
 }
-
-# functions to add
-# 1. list based on date
-# 2. list first n, last n files
-# 3. list i to j
-# 4. display info of all listed data ( pipelining to internat cat function )
