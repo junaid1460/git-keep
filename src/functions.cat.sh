5ec883c0d9c33f00079701b0b4306d87809d53de
junaid <junaid1460@gmail.com>
2017-12-15 12:30:29 +0530

    M : added functions for `doc cat fd` command

--- src/functions.cat.sh
+++ src/functions.cat.sh
@@ -1,12 +1,30 @@
 # show file data
 
 showfile () {
-    # $1 : filepath
+    # $1 : file
     # $2 : commit
     filepath=$1
-    message="$(cat $filepath)"
+    # message="$()"
     colori $Yellow
     print $commit
     color $Off
-    print $message
+    # echo $message
+    cat $files_dir/$file
+}
+
+showFileWithNumber () {
+    number=$1
+    messages="$(cat $info_file)"
+    count=0
+    IFS=$'\n'
+    for i in $messages;do
+        IFS=, read -r file commit <<< "$i"
+        if [ "$count" == "$number" ]
+        then 
+            showfile $file $commit
+            return
+        fi
+        count=$(($count+1))
+        IFS=$'\n'
+    done
 }
