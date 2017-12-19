dbf283a0174d79f363d98e41b93bd80b8a7e8292
junaid <junaid1460@gmail.com>
2017-12-19 10:08:42 +0530

    moved repreating code to two separate functions.

--- src/helpers.sh
+++ src/helpers.sh
@@ -29,3 +29,40 @@ commit? () {
         echo "committed chages"
     fi
 }
+
+getFile () {
+    number=$1
+    export currentCommit='None'
+    export currentFile='None'
+    messages="$(cat $info_file)"
+    count=0
+    IFS=$'\n'
+    for i in $messages;do
+        IFS=, read -r file commit <<< "$i"
+        if [ "$count" == "$number" ]
+        then 
+            export currentFile=$file
+            export currentCommit=$commit
+            return
+        fi
+        count=$(($count+1))
+        IFS=$'\n'
+    done
+}
+
+callOnFile () {
+    # $1 : filenumber
+    # $2 : func name
+    # $3 : error message
+    number=$1
+    func=$2
+    errorMessage=$3
+    getFile $number
+    if [ "$currentFile" == "None" ]
+    then
+        colori $Red
+        print "$errorMessage"
+    else
+        $func $currentFile $currentCommit $number
+    fi
+}
