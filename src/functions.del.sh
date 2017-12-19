428681c96678e255f3b7e49bead71330f1dcd08d
junaid <junaid1460@gmail.com>
2017-12-19 11:43:46 +0530

    Implement delete functionality

--- src/functions.del.sh
+++ src/functions.del.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 # functions related to deleting a file
 
+deleteFromInfo () {
+    number=$1
+    infos="$(cat $info_file)"
+    echo -n '' > $info_file
+    IFS=$'\n'
+    count=0
+    for line in $infos;do
+        if [ "$count" != "$number" ]
+        then
+            echo $line
+        fi
+        count=$(($count + 1))
+    done
+} >> $info_file
+
+
+
 confirmDeletion () {
+    file=$file
     commit=$2
     number=$3
     colori $Red 
     printi "Delete record "
     colori $Off
-    print "$commit?"
+    printi "$commit?[yN]"
+    read choice
+    if [ "$choice" == "y" ]
+    then
+        rm $files_dir/$file
+        deleteFromInfo $number
+        updateREADME "Deleted record: $commit."
+    fi
 }
 
 deleteWithNumber () {
